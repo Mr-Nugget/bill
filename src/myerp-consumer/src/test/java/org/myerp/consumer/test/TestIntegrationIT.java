684c3ea69e5dd6bf3f555fd66c9b88d95b2c4d2f
Titouan RAIMBAULT <t_raimbault@yahoo.fr>
2019-02-19 13:04:32 +0100

    Changement du profil IT, test de build sur jenkins

---
+++ src/myerp-consumer/src/test/java/org/myerp/consumer/test/TestIntegrationIT.java
@@ -0,0 +1,11 @@
+package org.myerp.consumer.test;
+
+import org.junit.Test;
+
+public class TestIntegrationIT {
+	
+	@Test
+	public void test1() {
+		System.out.println("\n\nBonjour\n\n");
+	}
+}
--- src/myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/TestIntegrationTempoIT.java
+++
@@ -1,9 +0,0 @@
-package com.dummy.myerp.model.bean.comptabilite;
-
-
-public class TestIntegrationTempoIT {
-	
-	public void test1(){
-		System.out.println("Bonjour");
-	}
-}
